873a13bb6af93cd3e26a643494a51fa75eab0fab
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-05 23:51:40 +0800

    this and dot notation

---
+++ dot.js
@@ -0,0 +1,13 @@
+const person = {
+  name: 'kodom ali',
+  job: 'badam khai',
+  3: 'third',
+  'add-ress': 'kola bagan'
+}
+
+const prop = 'job';
+console.log(person.job);
+console.log(person['job']);
+console.log(person[prop]);
+console.log(person['3']);
+console.log(person["add-ress"]);
